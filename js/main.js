26d23d721d304ff53d12f2fa82eae7407e9d0b9d
henriqueangelin <zhenrytm.fps@gmail.com>
2021-04-11 17:27:48 -0300

    Página pedidos

---
+++ js/main.js
@@ -0,0 +1,10 @@
+let serviço = document.getElementsByClassName('navserviços')[0]
+
+
+function services(){
+    serviço.style.display = 'block'
+}
+
+function fechar(){
+    serviço.style.display = 'none'
+}
